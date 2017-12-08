4edfc3832612bdb7ddd004910ece973a6187b5ab
user <user@localhost.localdomain>
2017-12-07 19:03:27 -0500

    added amq install script

---
+++ files/install_amq.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+#ActiveMQ Installation:
+wget http://mirror.cogentco.com/pub/apache//activemq/5.15.2/apache-activemq-5.15.2-bin.tar.gz
+tar xzvf apache-activemq-5.15.2-bin.tar.gz
+mv apache-activemq-5.15.2 /opt
+adduser activemq -r -U
+ln -s /opt/apache-activemq-5.15.2/ /opt/activemq
+chown -R activemq:activemq /opt/activemq
+chown -R activemq:activemq /opt/apache-activemq-5.15.2
+ln -s /opt/activemq/bin/activemq /etc/init.d/activemq
+chkconfig --add activemq
+#set default TTL to 5 mins
+sed -i '/<broker xmlns="http:\/\/activemq.apache.org\/schema\/core" brokerName="localhost" dataDirectory="${activemq.data}">/a \ \t<plugins>\n\t\t<!-- If not already set, set ttl to 5 minutes -->\n\t\t<timeStampingBrokerPlugin zeroExpirationOverride="300000"\/>\n\t<\/plugins>' /opt/activemq/conf/activemq.xml
+#Change config to use non-root user
+sed -i 's/ACTIVEMQ_USER=""/ACTIVEMQ_USER="activemq"/' /opt/activemq/bin/env
+#Open firewall ports:
+firewall-cmd --zone=public --add-port=61616/tcp --permanent
+firewall-cmd --zone=public --add-port=61617/tcp --permanent
+firewall-cmd --zone=public --add-port=8161/tcp --permanent
+firewall-cmd --zone=public --add-port=8162/tcp --permanent
+firewall-cmd --reload
+#Start the service:
+systemctl start activemq
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,20 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'wget'
+
+#get amq file
+cookbook_file '/tmp/install_amq.bash' do
+  source 'install_amq.bash'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+#install amq
+execute 'execute install_amq.bash' do
+ action :run
+ command '/tmp/install_amq.bash'
+end
