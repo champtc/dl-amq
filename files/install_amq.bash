a55662d4013bd16748768782aec09832d21e94ec
peterneorr <peterneorr@gmail.com>
2017-12-08 11:05:03 -0800

    updated amq path

--- files/install_amq.bash
+++ files/install_amq.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 #ActiveMQ Installation:
-wget http://champtc.com/amq/activemq/5.15.2/apache-activemq-5.15.2-bin.tar.gz
+wget http://champtc.com/amq/apache-activemq-5.15.2-bin.tar.gz
 tar xzvf apache-activemq-5.15.2-bin.tar.gz
 mv apache-activemq-5.15.2 /opt
 adduser activemq -r -U -m
